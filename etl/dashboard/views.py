25a3807d68981e157560b4b881a6e76ead837919
Sebastian Łach <root@slach.eu>
2019-10-23 18:01:34 +0200

    Process chart data

--- etl/dashboard/views.py
+++ etl/dashboard/views.py
@@ -9,11 +9,23 @@ def index(request):
         'http://adverity-challenge.s3-website-eu-west-1.amazonaws.com/'
         'DAMKBAoDBwoDBAkOBAYFCw.csv'
     )
+
     frame = reader.get_data_frame()
     datasources = list(set(frame['Datasource']))
     campaigns = list(set(frame['Campaign']))
+
+    result = frame
+    if datasources_values:
+        result = frame[(frame.Datasource.isin(datasources_values))]
+    if campaigns_values:
+        result = frame[(frame.Campaign.isin(campaigns_values))]
+
+    clicks = result[['Date', 'Clicks']].values.tolist()
+    impressions = result[['Date', 'Impressions']].values.tolist()
+
     context = dict(
-        frame=frame,
+        clicks=clicks,
+        impresions=impressions,
         datasources_options=datasources,
         datasources_values=datasources_values,
         campaigns_options=campaigns,
